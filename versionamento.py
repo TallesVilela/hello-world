e7ec2fe96f5c9e761d095ba4becae293c24a3200
TallesVilela <126349701+TallesVilela@users.noreply.github.com>
2024-01-23 10:53:50 -0300

    Update versionamento.py

--- versionamento.py
+++ versionamento.py
@@ -2,9 +2,12 @@ print("Este aplicativo foi criado para estudos de Git")
 
 n = range(11)
 
+
+if n[10] < 10:
+    print("Cuidado com os índices")
+
 try:
     for i in n:
         print(i)
 except:
     print("Esta iteração falhou")
-
